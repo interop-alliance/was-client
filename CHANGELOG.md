c1e40750781fe5685e5304965df1d9d93d7b4e2d
Dmitri Zagidulin <dzagidulin@gmail.com>
2026-06-06 00:25:44 +0000

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# @interop/isomorphic-lib-template Changelog
+
+## 1.0.0 - TBD
+
+### Added
+
+- Initial commit.
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+/*!
+ * Copyright (c) 2026 Interop Alliance and Dmitri Zagidulin. All rights reserved.
+ */
+export { Example } from './Example.js'
